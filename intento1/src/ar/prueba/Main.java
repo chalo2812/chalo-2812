d03b2f76c97ca88b98a758a595fb0bc49794fc31
gonzalosola <gonzalosola@e290e524-dcc9-e6dd-992a-3a6b9c64af2b>
2013-05-11 05:18:41 +0000

    Modificacion y creacion del ejecutable

--- intento1/src/ar/prueba/Main.java
+++ intento1/src/ar/prueba/Main.java
@@ -1,5 +1,14 @@
 package ar.prueba;
 
+/**
+ * Ejecutable
+ * @author Gonzalo Sola - gonzalosola@gmail.com
+ *
+ */
 public class Main {
 
+	public static void main(String arg[]) {
+		System.out.println("prueba");
+	}
+	
 }
