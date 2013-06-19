606a7de75a0099cdf8a9ebee9ef440e534886cea
gonzalosola@gmail.com <gonzalosola@gmail.com@e290e524-dcc9-e6dd-992a-3a6b9c64af2b>
2013-06-19 05:03:18 +0000

    Unificando versiones.

---
+++ trunk/src/tp/procesadores/compilador/CompilerAndGenerator.java
@@ -0,0 +1,86 @@
+package tp.procesadores.compilador;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import tp.procesadores.analizador.sintactico.SintacticAnalyzer;
+import tp.procesadores.compilador.generadorcodigo.constantes.Constants;
+
+public class CompilerAndGenerator extends Compiler {
+
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out
+					.println("Este compilador del lenguaje LEB precisa de 1 parametro: "
+							+ "\n\t1.- origen: archivo con el codigo fuente creado en LEB "
+							+ "\n\nEJ: java -jar compilador.jar .\\fuente.leb "
+							+ "\n\nLa salida quedara guardada en un archivo programaObjeto.asm en el mismo directorio donde se encuentra el jar.");
+		} else {
+			SintacticAnalyzer sa = new SintacticAnalyzer(args[0]);
+			sa.Compilar();
+			CompilerAndGenerator generar = new CompilerAndGenerator();
+			if( sa.getEstadoAnalisis())
+				generar.transformarAssembler(sa, args[0]);
+			
+		}
+	}
+
+	private void transformarAssembler(SintacticAnalyzer sa, String file) {
+		borrarASM(file);
+		crearASM(file);
+	}
+
+	private void crearASM(String file) {
+		File output = new File(file + ".asm");
+
+		PrintWriter fileWriter;
+		try {
+			fileWriter = new PrintWriter(output);
+			String resultado = armadoEstructuraASM();
+
+			fileWriter.write(resultado);
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private String armadoEstructuraASM() {
+		StringBuilder mensaje = new StringBuilder();
+		mensaje.append(encabezadoASM());
+		mensaje.append(parteDelArchEntradaASM());
+		mensaje.append(finASM());
+		return mensaje.toString();
+	}
+
+	private String finASM() {
+		StringBuilder mensaje = new StringBuilder();
+		return mensaje.toString();
+	}
+
+	private String parteDelArchEntradaASM() {
+		StringBuilder mensaje = new StringBuilder();
+		return mensaje.toString();
+	}
+
+	private String encabezadoASM() {
+		StringBuilder mensaje = new StringBuilder();
+		mensaje.append(Constants.encabezado);
+		mensaje.append(Constants.writeSTR);
+		return mensaje.toString();
+	}
+
+	private void borrarASM(String file) {
+		File archivo = new File(file);
+		if (archivo.exists()) {
+			if (archivo.getName().endsWith(".asm")) {
+				if (archivo.delete()) {
+					System.out.println("Se borro el archivo " + archivo
+							+ " exitosamente.");
+				}
+			}
+		}
+	}
+}
