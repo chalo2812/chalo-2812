e5dc67b1460b83adb32657c950b5b294f41e7df7
gonzalosola@gmail.com <gonzalosola@gmail.com@e290e524-dcc9-e6dd-992a-3a6b9c64af2b>
2013-06-20 05:15:07 +0000

    Reestructuracion

--- trunk/tpcompiladores/src/tp/procesadores/analizador/lexico/tokens/visitor/ProductionVisitor.java
+++
@@ -1,8 +0,0 @@
-package tp.procesadores.analizador.lexico.tokens.visitor;
-
-import tp.procesadores.analizador.sintactico.producciones.ProduccionC;
-
-public interface ProductionVisitor {
-
-	public Object visit(ProduccionC produccionC);
-}
