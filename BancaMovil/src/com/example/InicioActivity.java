18fbf5aed01a4640b9e37f49547b9dc41b8eb5bc
gonzalosola <gonzalosola@e290e524-dcc9-e6dd-992a-3a6b9c64af2b>
2013-07-26 16:48:02 +0000

    Commit Inicial

---
+++ BancaMovil/src/com/example/InicioActivity.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.support.v4.app.NavUtils;
+
+public class InicioActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ImageView img = (ImageView)findViewById(R.drawable.imagen_credicoop);
+		OnClickListener imagen = new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+			}
+		};
+		//imagen.onClick(img);
+		setContentView(R.layout.activity_inicio);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_inicio, menu);
+		return true;
+	}
+	
+	protected void onCreate(){
+		
+	}
+
+
+
+}
---
+++ BancaMovil/src/com/example/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ImageView img= (ImageView)findViewById(R.drawable.credicoop);
+		//img.setImageResource(R.drawable.credicoop);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
